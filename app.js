285207737e178ee64f2d7b0c96a0daa272732c45
Siddharth Choudhari <choudharishiddharth@gmail.com>
2023-01-27 22:03:59 +0530

    add new feature

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ require('express-async-errors');
 
 const express = require("express");
 const app = express();
+
 const cors = require('cors')
 const connectDB = require("./db/connect");
 const tasksRouter = require("./routes/tasks");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //middleware
+app.use(express.static("public"))
 app.use(express.json());
 app.use(cors())
 
@@ -17,6 +20,10 @@ var corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
 
 //routes
 app.use("/api/v1/tasks", tasksRouter);
